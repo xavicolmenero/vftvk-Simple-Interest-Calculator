7f9efecf577af03b90e2cb2dc03060877dfa8120
Xavier Colmenero <xavicolmenero@gmail.com>
2021-10-17 09:17:32 +0000

    Final project commit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //Calculates the amount that the user will receive. 
+    //Shows a text with the with the provided values by the user and the amount received.
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <strong>"+principal+"</strong>,\<br\>at an interest rate of <strong>"+rate+"%</strong>\<br\>You will receive an amount of <strong>"+interest+"</strong>,\<br\>in the year <strong>"+year+"<strong>\<br\>"
 }
+       
+function updateRate() 
+{
+    //To refresh the value shown next to the range scroll
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval;
+}
+
+function checkPrincipal()
+{
+    //Will check if the principal value is greater than 0. Must convert to an integer to compare to 0.
+    var principalValue = parseInt(document.getElementById("principal").value);
+    if (principalValue<=0) alert("Amount value not valid. Must be greater than 0");
+    //Return focus to principal
+    principal.focus();
+}   
         
